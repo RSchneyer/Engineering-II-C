857901422f741d571fd0f1ca7d46425734f3de1a
Reid S <reschneyer89@gmail.com>
2018-11-06 17:40:15 +0000

    Added folder for bank project, and gitignore file

---
+++ Project Files/main.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+#include <time.h>
+
+//I'm saving myself from typing 5 letters each time. It adds up, I swear
+#define OUTFILE "xactlog.txt"
+
+int main(void){
+    double balance, APR;
+    
+    
+   return 0; 
+}
+
+
+void logDeposit(double deposit){}
+
+void logWithdrawal(double withdrawal){}
+
+void logInterest(double interest){}
+
+void logFee(double fee){}
+
+void logClosure(){}
