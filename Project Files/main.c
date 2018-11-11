883d2a6fdeba71b766dac255f06b1d7537260bad
Reid Schneyer <reschneyer89@gmail.com>
2018-11-10 21:11:59 -0600

    depositing and withdrawing works, feelsgoodman

--- Project Files/main.c
+++ Project Files/main.c
@@ -15,19 +15,26 @@ void createFileHeader(){
     fclose(outfile);
 }
 
-void logDeposit(double depositAmount, double currentBalance){
-    if(depositAmount<0.0){
-        return;
-    }
-    double newBalance = depositAmount + currentBalance;
+void logAccountEvent(double balanceChange, double currentBalance){
+    double newBalance = balanceChange + currentBalance;
     FILE * outfile;
     outfile = fopen(OUTFILE_NAME, "a");
     if(outfile == NULL){
         printf("\nCan't open logfile!\n");
     }
-    fprintf(outfile, "deposit,  %.2f,  %.2f,\n", depositAmount, newBalance);
+
+    if(balanceChange >= 0.0){// Deposit is occuring
+        fprintf(outfile, "deposit,  %.2f,  %.2f,\n", balanceChange, newBalance);
+    } else if(balanceChange < 0.0){
+        fprintf(outfile, "withdrawl,  %.2f,  %.2f,\n", balanceChange, newBalance);
+    }
+
     fclose(outfile);
 }
+
+
+
+
 
 // Prompts user with message asking how much they'd like to deposit/withdraw, then returns that amount
 // Prompt type should be 0 if the balance change is a deposit, and anything else if it's a withdrawl
@@ -39,17 +46,21 @@ double changeBalance(int promptType, double currentBalance){
             printf("Deposit how much? ");
             scanf("%lf", &balanceChange);
         } while(balanceChange<=0.0);
-        logDeposit(balanceChange, currentBalance);
+        logAccountEvent(balanceChange, currentBalance);
     } 
     else{
         do{
             printf("Withdraw how much? ");
             scanf("%lf", &balanceChange);
-        } while((currentBalance-balanceChange)>0.0);
+        } while((currentBalance-balanceChange)<0.0);
         balanceChange *= -1;
+        logAccountEvent(balanceChange, currentBalance);
     }
     return balanceChange;
 }
+
+
+
 
 //Contains the 7 or so printf statements for the system menu
 void printMenu(){
@@ -62,7 +73,6 @@ void printMenu(){
     printf("5 - Close your account\n");
     printf("Please enter your choice: ");
 }
-
 //Displays the menu from printMenu(), and returns an int between 1 and 5 based on user input
 int getMenuSelection(){
     int selection = 0;
@@ -74,15 +84,25 @@ int getMenuSelection(){
 
 }
 
-double initAccount(){
-    double balance;
-    printf("Welcome to Reid's Banking Simulator! Please enter your starting balance: ");
-    scanf("%lf", &balance);
-    logDeposit(balance, 0);
-    return balance;
+void initAccount(double *initDepositAndAPR){
+    double balance, apr;
+    printf("Welcome to Reid's Banking Simulator! ");
+    do{
+        printf("Please enter your starting balance: ");
+        scanf("%lf", &balance);
+        printf("\n");
+    } while(balance<0.0);
+
+    do{
+        printf("Please enter your APR: ");
+        scanf("%lf", &apr);
+        printf("\n");
+    } while(apr<0.0);
+
+    initDepositAndAPR[0] = balance;
+    initDepositAndAPR[1] = apr;
+    createFileHeader();
 }
-
-
 
 
 
@@ -92,13 +112,20 @@ int main(void){
     //Delete old log file
     //Might not be the best way to do this: https://stackoverflow.com/questions/19913446/why-should-the-system-function-be-avoided-in-c-and-c
     int status = system("rm xactlog.txt");
-    if(status == 0){//If the file got deleted 
-        createFileHeader();
-
+    // int status = 0;
+    if(status == 0){//If the file got deleted succesfully
         int actionSelection = 1;
-        double balance, APR;
+        // This part is a bit confusing, I wanted one function for setting up an account
+        // C functions can't straight up return arrays, so initAccount() takes an array pointer as an argument
+        // Basically, I wanted to return two values from one function, and this is my solution
+        double balance, apr;
+        double balanceAndAprArray[2] = {0};
+        initAccount(balanceAndAprArray);
+        // Too bad C doesn't have array destructuring :/
+        balance = balanceAndAprArray[0];
+        apr = balanceAndAprArray[1];
 
-        balance = initAccount();
+        logAccountEvent(balance, 0);
 
         while(actionSelection>0 && actionSelection<5){//While the user has chosen a number between 1-4
             actionSelection = getMenuSelection();
@@ -111,7 +138,8 @@ int main(void){
                     break;
                 case 2:
                     //Make a withdrawl
-                    printf("Function not available right now\n");
+                    balance += changeBalance(1, balance);
+                    // printf("Function not available right now\n");
                     break;
                 case 3:
                     //Calculate interest
